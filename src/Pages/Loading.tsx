16f62dc91895335f26405154f9ce7a2392f692a3
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-03-09 13:46:46 +0100

    Update loading css

--- src/Pages/Loading.tsx
+++ src/Pages/Loading.tsx
@@ -4,8 +4,10 @@ export default function Loading(): JSX.Element {
   return (
     <Center height={"100vh"}>
       <VStack>
-        <Spinner color="primary.500" size="xl" />
-        <Text fontSize={12}>LOADING...</Text>
+        <Spinner color="black" size="xl" thickness="8px" />
+        <Text fontSize={12} fontWeight={"bold"}>
+          LOADING...
+        </Text>
       </VStack>
     </Center>
   );
